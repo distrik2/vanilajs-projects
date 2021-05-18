274f498a8ddb48d18f126476732a7ef3d821c181
Distrik <kuanbek012@gmail.com>
2021-05-18 17:54:15 +0500

    Todo App

--- Todo App/js/script.js
+++ Todo App/js/script.js
@@ -2,21 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const input = document.querySelector('input[type="text"]');
     const todoList = document.querySelector('.todo');
-    const chekbox = document.querySelectorAll('input[type="checkbox"]');
     const form = document.querySelector("form");
 
-    let items = JSON.parse(localStorage.getItem("todoTest"));
-    let storageAdd = [];
-    let copyStorage = [];
-    if (items != null) { storageAdd = copyStorage.concat(items); }
+    let items;
+    if (localStorage.getItem("todoTest") == null) {
+        localStorage.setItem("todoTest", JSON.stringify([{}]));
+    } else {
+        items = JSON.parse(localStorage.getItem("todoTest"));
+    }
 
     function loadTodo() {
         if (localStorage.getItem('todoTest')) {
-            items.forEach(i => {
+            items.forEach((i, item) => {
                 todoList.innerHTML += `
                     <li>
-                        <input type="checkbox" name="" id="">
-                        <label for="${i.id}">${i.title}</label>
+                        <input type="checkbox" ${items[item].done}>
+                        <label>${i.title}</label>
                         <label type="delete" for="">Удалить</label>
                     </li>
                 `;
@@ -24,13 +25,41 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+
+    document.querySelectorAll('li').forEach((j) => {
+        console.log(j.children[0]);
+    });
+
+
+    function doneElement() {
+        document.querySelectorAll('li').forEach((i, item) => {
+            i.addEventListener("change", (e) => {
+
+                if (items[item].done === "checked") {
+                    items[item].done = "false";
+                } else {
+                    items[item].done = "checked";
+                }
+
+                localStorage.setItem("todoTest", JSON.stringify(items));
+            });
+        });
+    }
+
     function deleteElement() {
         document.querySelectorAll('label[type="delete"]').forEach((i, item) => {
             i.addEventListener("click", (e) => {
                 i.parentElement.remove();
-                
-                storageAdd.splice(item, 1);
-                localStorage.setItem("todoTest", JSON.stringify(storageAdd));
+
+                items = [];
+                document.querySelectorAll('li').forEach((j) => {
+                    items.push({
+                        title: j.children[1].innerHTML,
+                        done: j.children[0].checked
+                    });
+                });
+
+                localStorage.setItem("todoTest", JSON.stringify(items));
             });
         });
     }
@@ -40,20 +69,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
         let todoObj = {
             title: "",
-            done: "false",
-            id: 0
+            done: "false"
         };
 
         if (input.value !== "" && input.value !== " ") {
             todoObj.title = input.value;
-            todoObj.id = storageAdd.length;
-            storageAdd.push(todoObj);
+            items.push(todoObj);
 
-            localStorage.setItem("todoTest", JSON.stringify(storageAdd));
+            localStorage.setItem("todoTest", JSON.stringify(items));
             todoList.innerHTML += `
                 <li>
-                    <input type="checkbox" name="" id="">
-                    <label for="${todoObj.id}">${todoObj.title}</label>
+                    <input type="checkbox">
+                    <label>${todoObj.title}</label>
                     <label type="delete" for="">Удалить</label>
                 </li>
             `;
@@ -62,9 +89,11 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("але");
         }
 
+        doneElement();
         deleteElement();
     });
 
     loadTodo();
+    doneElement();
     deleteElement();
 });
